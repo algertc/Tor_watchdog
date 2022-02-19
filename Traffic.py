499bf96c0762cf52d2bf219fb1817ca2f684de0a
charliealgert <algertc@gmail.com>
2022-02-19 15:25:43 -0800

    float fix

--- Traffic.py
+++ Traffic.py
@@ -23,8 +23,8 @@ class Traffic:
     with Controller.from_port(port=9051) as controller:
       # access tor hash password from config.ini
       controller.authenticate(getAuth.hash())
-      self.bytes_read = float(controller.get_info("traffic/read") / 1000000000)
-      self.bytes_written = float(controller.get_info("traffic/written") / 1000000000)
+      self.bytes_read = float(controller.get_info("traffic/read")) / 1000000000
+      self.bytes_written = float(controller.get_info("traffic/written")) / 1000000000
       #timestamp for sorting and logging in SQL
       self.timeStamp = "%s/%s/%s" % (datetime.today().month, datetime.today().day, datetime.today().year)
 
