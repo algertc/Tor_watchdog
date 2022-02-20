df1ca8dd32282ca2eeb76f7bbb10aad50adea5a8
charliealgert <algertc@gmail.com>
2022-02-20 02:43:40 -0800

    loop fix

--- Report.py
+++ Report.py
@@ -1,15 +1,14 @@
-#mysql decorator
-def interface(func):
-  def wrapper(config, up, down, timeStamp):
-    #log to mysql DB with HH:MM:SS - MM/DD/YYYY - UP - DOWN
-    import logger
-    logger.log(timeStamp, up, down)
-    tmp = config
-  return wrapper
+def notif(func):
+    def wrap(*args, **kwargs):
+        print("Handling Report")
+        func(*args, **kwargs)
+    return wrap
 
-@interface
+@notif
 def handler(config, up, down, timeStamp):
     import mailer
+    import logger
+    logger.log(timeStamp, up, down)
     #sent to mailer
     tmp = timeStamp
     mailer.sendMail(config, down, up)
