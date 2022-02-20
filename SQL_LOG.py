a828b21c61170091504e14b99abe3bba2e3d00d2
charliealgert <algertc@gmail.com>
2022-02-20 03:17:03 -0800

    truncate float for sql

--- SQL_LOG.py
+++ SQL_LOG.py
@@ -13,6 +13,7 @@ def post(time, up, down):
         )
     cursor = db.cursor() #pointer in db
     cursor.execute("USE tor_logging;") #tor_logging - db name
-    #truncate float for sql limit
-    cursor.execute("INSERT INTO log (timestamp, bytesUP, bytesDOWN) VALUES (%s,%s,%s);" % (time, up[0:5], down[0:5]))
+    sql = "INSERT INTO log (timestamp, bytesUP, bytesDOWN) VALUES (%s, %s)"
+    val = [time, str(up[0:5]), str(down[0:5])]
+    cursor.executemany(sql, val)
     db.commit()
