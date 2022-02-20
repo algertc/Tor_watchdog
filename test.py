f7db421449c73520de3515688010ea767f0edfb7
charliealgert <algertc@gmail.com>
2022-02-19 18:46:14 -0800

    ob

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-db = mysql.connector.connect(host=str(config['MYSQL']['host']), user=str(config['MYSQL']['user']), passwd=(base64.b64decode(str(config['MYSQL']['passwd']).encode('ascii')).decode('ascii')))
-#db = mysql.connector.connect(host=192.168.0.107, )
+#db = mysql.connector.connect(host=str(config['MYSQL']['host']), user=str(config['MYSQL']['user']), passwd=(base64.b64decode(str(config['MYSQL']['passwd']).encode('ascii')).decode('ascii')))
+db = mysql.connector.connect(host= "192.168.0.107", user="charles", passwd = str(input("password: ")))
 cursor = db.cursor()
 cursor.execute("show databases")
 for i in cursor:
